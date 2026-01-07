f2880f8b68228832c7f2d8d3a78c7b4953a9b32a
VULF-Int <Vulfinteractive@gmail.com>
2026-01-08 02:28:02 +0530

    Fix Cart export

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,5 +2,5 @@ export { MenuBrowser } from './MenuBrowser';
 export { ItemDetail } from './ItemDetail';
 export { LoadingScreen } from './LoadingScreen';
 export { WelcomeScreen } from './WelcomeScreen';
-export { Cart, CartButton } from './Cart';
+export { CartButton } from './Cart';
 export { ModelThumbnail } from './ModelThumbnail';
